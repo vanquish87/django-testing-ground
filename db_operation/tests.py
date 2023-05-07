9bc3bd18cc6da1fbbdcecc97dd8afdce60681e95
Jasmeet Singh <jasmeet.singh@essential-net.com>
2023-05-08 00:43:04 +0530

    bulk add and testing
    - added decouple for security of keys in settins.py
    - added a postgres db - 'elephantsql' --- commented
    - added new function add_random_records_bulk in utils.py
    - created new test for add_random_records_bulk to test differenct batch size

--- db_operation/tests.py
+++ db_operation/tests.py
@@ -1,13 +1,26 @@
+from typing import Any
 from django.test import TestCase
-from .utils import add_random_records
+from .utils import add_random_records, add_random_records_bulk
 import time
 
 class TestAddRandomRecords(TestCase):
+    def setUp(self):
+         self.num_records = 400000
+         self.batch_size = 120
+         
 
-    def test_add_random_records_time(self):
+    def test_add_random_records_time_taken(self):
         start_time = time.perf_counter()
-        add_random_records(num_records=10000)
+        add_random_records(num_records=self.num_records)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        print(total_time)
-        self.assertLessEqual(total_time, 10.0, "Adding 10000 random records took more than 10 seconds")
+        print(f'Single Insert took {total_time}')
+
+
+    def test_add_random_records_bulk_time_taken(self):
+        for i in range(1, self.batch_size + 1):
+            start_time = time.perf_counter()
+            add_random_records_bulk(num_records=self.num_records, batch_size=i)
+            end_time = time.perf_counter()
+            total_time = end_time - start_time
+            print(f'Bulk Insert took {total_time} with the batch_size of {i}')
--- db_operation/utils.py
+++ db_operation/utils.py
@@ -2,7 +2,9 @@ import random
 from datetime import timedelta
 from db_operation.models import RecordMapping
 
+from django.db import transaction
 from django.utils import timezone
+
 
 def add_random_records(num_records):
     for i in range(num_records):
@@ -22,3 +24,37 @@ def add_random_records(num_records):
             total_outgoing_bytes=random.randint(100000, 100000000),
         )
         record.save()
+
+
+def add_random_records_bulk(num_records, batch_size):
+    # Create a list to hold the records
+    records = []
+
+    # Generate the records and append them to the list
+    for i in range(num_records):
+        record = RecordMapping(
+            apn=f"apn{i}",
+            country_id=random.randint(1, 100),
+            total_incoming_bytes=random.randint(100000, 100000000),
+            close_time_on=timezone.now() - timedelta(days=random.randint(0, 365)),
+            ip_address=f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}",
+            ipv6_address=f"{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}:{random.randint(0, 65535):04x}",
+            ipv6_length=random.randint(1, 128),
+            external_id=random.randint(1000000000, 9999999999),
+            provider_id=random.randint(1, 10),
+            start_time_on=timezone.now() - timedelta(days=random.randint(0, 365)),
+            total_bytes=random.randint(100000, 100000000),
+            total_outgoing_bytes=random.randint(100000, 100000000),
+        )
+        records.append(record)
+
+        # Insert the records in batches
+        if len(records) == batch_size:
+            with transaction.atomic():
+                RecordMapping.objects.bulk_create(records)
+            records = []
+
+    # Insert any remaining records
+    if records:
+        with transaction.atomic():
+            RecordMapping.objects.bulk_create(records)
